c318d2bcf1772aa84202164eb0d9c3c05eeed98f
Joran Daniël <124999321+JoranDaniel@users.noreply.github.com>
2023-04-28 10:48:11 +0200

    Update Program.cs

--- Blackjack_opdracht_Joran/Program.cs
+++ Blackjack_opdracht_Joran/Program.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
-class Program
+class Program // dit is de class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) // dit is de methode Main, deze methode wordt als eerste uitgevoerd
     {
-        Game game = new Game();
-        game.Start();
-     
+        Game game = new Game(); // hiermee wordt de class Game aangeroepen
+        game.Start(); // hiermee wordt de methode Start aangeroepen
+        Console.WriteLine("Bedankt voor het spelen!");
     }
 }
