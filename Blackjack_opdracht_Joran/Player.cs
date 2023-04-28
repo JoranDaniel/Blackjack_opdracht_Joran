9e3039e534ee8349f782dd725518bd70b01fa638
Joran Daniël <124999321+JoranDaniel@users.noreply.github.com>
2023-04-28 10:45:45 +0200

    Update Player.cs

--- Blackjack_opdracht_Joran/Player.cs
+++ Blackjack_opdracht_Joran/Player.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 
-public class Player
+public class Player // we maken een class aan genaamd Player, deze class is voor de speler
 {
-    public int Score { get; set; }
+    public int Score { get; set; } // dit is een public int genaamd Score, deze int is voor de score van de speler
 
-    public void ReceiveCard(int value)
+    public void ReceiveCard(int value) // dit is een public void genaamd ReceiveCard, deze void zorgt ervoor dat de speler een kaart krijgt
     {
-        Score += value;
+        Score += value; // dit zorgt ervoor dat de waarde van de kaart wordt opgeteld bij de score van de speler
     }
 }
